800524244d883a59c8343954f0bfa19c601b64dc
theuserbl <theuserbl@hotmail.com>
2016-11-08 02:12:00 +0100

    Create CoreConstants.java

---
+++ jd/core/CoreConstants.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core;
+
+public class CoreConstants
+{
+    public static final String JD_CORE_VERSION = "0.7.1";
+    public static final int JAVA_MAGIC_NUMBER = 0xCAFEBABE;
+}
