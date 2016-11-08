af182c6bb2bec9ff0e41a6a3284a517209cfa40a
theuserbl <theuserbl@hotmail.com>
2016-11-08 10:29:47 +0100

    Create UtilConstants.java

---
+++ jd/core/util/UtilConstants.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.util;
+
+public class UtilConstants
+{
+    public static final int INVALID_INDEX = -1;
+    public static final int INVALID_OFFSET = -1;
+}
