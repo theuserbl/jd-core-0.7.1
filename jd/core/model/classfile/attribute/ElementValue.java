38324694e15f04363d381aeb0ec5b54cdbc45a78
theuserbl <theuserbl@hotmail.com>
2016-11-09 01:20:56 +0100

    Create ElementValue.java

---
+++ jd/core/model/classfile/attribute/ElementValue.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.model.classfile.attribute;
+
+
+public class ElementValue
+{
+    public final byte tag;
+
+    public ElementValue(byte tag)
+    {
+        this.tag = tag;
+    }
+}
