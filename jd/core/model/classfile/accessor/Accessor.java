90ff600ff4eb461c620fd37064055a14bbfd0e67
theuserbl <theuserbl@hotmail.com>
2016-11-12 00:26:40 +0100

    Create Accessor.java

---
+++ jd/core/model/classfile/accessor/Accessor.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.model.classfile.accessor;
+
+public class Accessor
+{
+    public final byte tag;
+
+    protected Accessor(byte tag)
+    {
+        this.tag = tag;
+    }
+}
