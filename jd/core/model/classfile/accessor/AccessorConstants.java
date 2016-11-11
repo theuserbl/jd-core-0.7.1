61c65f2bcb3c27b0916ccec6b6a61c49def71c3a
theuserbl <theuserbl@hotmail.com>
2016-11-12 00:29:24 +0100

    Create AccessorConstants.java

---
+++ jd/core/model/classfile/accessor/AccessorConstants.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.model.classfile.accessor;
+
+
+public class AccessorConstants
+{
+    public static final byte ACCESSOR_GETSTATIC = 1;
+    public static final byte ACCESSOR_PUTSTATIC = 2;
+    public static final byte ACCESSOR_GETFIELD = 3;
+    public static final byte ACCESSOR_PUTFIELD = 4;
+    public static final byte ACCESSOR_INVOKEMETHOD = 5;
+}
