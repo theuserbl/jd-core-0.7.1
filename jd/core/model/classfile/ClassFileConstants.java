f05b72f00feffe0a8d9165a7127454925ec10555
theuserbl <theuserbl@hotmail.com>
2016-11-12 14:50:09 +0100

    Create ClassFileConstants.java

---
+++ jd/core/model/classfile/ClassFileConstants.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.model.classfile;
+
+
+public class ClassFileConstants
+{
+    public static final short ACC_PUBLIC = 1;
+    public static final short ACC_PRIVATE = 2;
+    public static final short ACC_PROTECTED = 4;
+    public static final short ACC_STATIC = 8;
+    public static final short ACC_FINAL = 16;
+    public static final short ACC_SYNCHRONIZED = 32;
+    public static final short ACC_SUPER = 32;
+    public static final short ACC_VOLATILE = 64;
+    public static final short ACC_BRIDGE = 64;
+    public static final short ACC_TRANSIENT = 128;
+    public static final short ACC_VARARGS = 128;
+    public static final short ACC_NATIVE = 256;
+    public static final short ACC_INTERFACE = 512;
+    public static final short ACC_ABSTRACT = 1024;
+    public static final short ACC_STRICT = 2048;
+    public static final short ACC_SYNTHETIC = 4096;
+    public static final short ACC_ANNOTATION = 8192;
+    public static final short ACC_ENUM = 16384;
+}
