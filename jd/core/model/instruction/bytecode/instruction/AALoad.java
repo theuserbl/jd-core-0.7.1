677d725707797e8eca3b7a12225804e0c3a67de2
theuserbl <theuserbl@hotmail.com>
2016-11-13 18:46:54 +0100

    Create AALoad.java

---
+++ jd/core/model/instruction/bytecode/instruction/AALoad.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.model.instruction.bytecode.instruction;
+
+import jd.core.model.classfile.ConstantPool;
+import jd.core.model.classfile.LocalVariables;
+
+
+
+public class AALoad
+  extends ArrayLoadInstruction
+{
+    public AALoad(int opcode, int offset, int lineNumber, Instruction arrayref, Instruction indexref)
+    {
+        super(opcode, offset, lineNumber, arrayref, indexref, null);
+    }
+
+
+    public String getReturnedSignature(ConstantPool constants, LocalVariables localVariables)
+    {
+        String signature = 
+          this.arrayref.getReturnedSignature(constants, localVariables);
+
+        if ((signature == null) || (signature.length() == 0) || 
+          (signature.charAt(0) != '[')) {
+            return null;
+        }
+        return signature.substring(1);
+    }
+}
