324d4be5c228a40c3383eed8d8e7c62e5244e8cd
theuserbl <theuserbl@hotmail.com>
2016-11-13 19:20:18 +0100

    Create ArrayStoreInstruction.java

---
+++ jd/core/model/instruction/bytecode/instruction/ArrayStoreInstruction.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.model.instruction.bytecode.instruction;
+
+import jd.core.model.instruction.bytecode.instruction.attribute.ValuerefAttribute;
+
+
+
+public class ArrayStoreInstruction
+  extends ArrayLoadInstruction
+  implements ValuerefAttribute
+{
+    public Instruction valueref;
+
+    public ArrayStoreInstruction(int opcode, int offset, int lineNumber, Instruction arrayref, Instruction indexref, String signature, Instruction valueref)
+    {
+        super(opcode, offset, lineNumber, arrayref, indexref, signature);
+        this.valueref = valueref;
+    }
+
+    public Instruction getValueref()
+    {
+        return this.valueref;
+    }
+
+    public void setValueref(Instruction valueref)
+    {
+        this.valueref = valueref;
+    }
+}
