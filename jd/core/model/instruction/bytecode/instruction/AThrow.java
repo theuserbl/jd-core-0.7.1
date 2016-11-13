2cb9ef5aacf5552a792e9bc0ad2022c687b29c1f
theuserbl <theuserbl@hotmail.com>
2016-11-13 19:54:07 +0100

    Create AThrow.java

---
+++ jd/core/model/instruction/bytecode/instruction/AThrow.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2008-2015 Emmanuel Dupuy
+ * This program is made available under the terms of the GPLv3 License.
+ */
+
+package jd.core.model.instruction.bytecode.instruction;
+
+import jd.core.model.classfile.ConstantPool;
+import jd.core.model.classfile.LocalVariables;
+
+
+public class AThrow
+  extends Instruction
+    {
+    public Instruction value;
+
+    public AThrow(int opcode, int offset, int lineNumber, Instruction value)
+    {
+    super(opcode, offset, lineNumber);
+        this.value = value;
+    }
+
+
+    public String getReturnedSignature(ConstantPool constants, LocalVariables localVariables)
+    {
+        return this.value.getReturnedSignature(constants, localVariables);
+    }
+}
